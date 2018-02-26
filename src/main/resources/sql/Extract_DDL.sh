f03a9f500aa09e04de11c9ca1e9ed4d30d160440
CamilleDesmots <farfadais71@yahoo.fr>
2018-02-26 23:33:46 +0100

    Correct the name of the jdbc pool name

---
+++ src/main/resources/sql/Extract_DDL.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Extract DDL of the Derby database
+
+if [$DERBY_HOME==""] then
+    export $DERBY_HOME="/Users/camilledesmots/GlassFish_Server/glassfish5/javadb/"
+fi 
+
+exec java -jar $DERBY_HOME\lib\derbyrun.jar dblook -d 'jdbc:derby://localhost:1527/GeigerCounter;user=GeigerCounter;password=GeigerCounter' -o GeigerCounter_extracted_DDL.sql -verbose
